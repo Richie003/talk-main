24823cd6aa6a962417a0249351a0cdc17f42af29
Dilosi Richfield <richiedilosi2003@gmail.com>
2025-08-26 14:35:53 +0100

    Update views.py

--- talkapp/views.py
+++ talkapp/views.py
@@ -8,38 +8,40 @@ from utils.helpers import send_email_in_thread, custom_response
 from rest_framework.generics import (
     GenericAPIView,
     CreateAPIView,
-    UpdateAPIView
+    UpdateAPIView,
+    RetrieveAPIView
 )
 from django.utils.timezone import now
 import datetime
-from drf_yasg.utils import swagger_auto_schema
+from rest_framework_simplejwt.views import TokenRefreshView
+from drf_spectacular.utils import extend_schema
 from django.conf import settings
 from .models import CustomUser, Individual, ServiceProvider, OneTimePassword
 from .serializers import (
-    CustomUserSerializer,
-    UserLoginSerializer,
-    SetNewPasswordSerializer,
-    ResendEmailActivationSerializer,
+    CustomUserSerializer, 
+    UserLoginSerializer, 
+    SetNewPasswordSerializer, 
+    ResendEmailActivationSerializer, 
     OTPVerificationSerializer,
     UpdateUserProfileSerializer,
     IndividualSerializer,
-    ServiceProvidersSerializer
+    ServiceProvidersSerializer,
 )
 from django.contrib.auth import get_user_model
 from django.contrib import auth
 import threading
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import exceptions
+from rest_framework_simplejwt.views import TokenRefreshView
 
 User = get_user_model()
-
 
 class CreateUserViewSet(CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [AllowAny]
+    @extend_schema(tags=["Students Auth"], operation_id="Student sign up")
 
-    @swagger_auto_schema(tags=["Students Auth"], operation_id="Student sign up")
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -62,13 +64,11 @@ class CreateUserViewSet(CreateAPIView):
             })
 
             subject = "One time PWD"
-            thread = threading.Thread(target=send_email_in_thread, args=(
-                subject, email_data, user.email))
+            thread = threading.Thread(target=send_email_in_thread, args=(subject, email_data, user.email))
             thread.start()
 
         except ObjectDoesNotExist:
-            raise exceptions.ValidationError(
-                {"error": ["User with this email not found"]})
+            raise exceptions.ValidationError({"error": ["User with this email not found"]})
         except Exception as e:
             return Response({
                 "status": "failed",
@@ -78,12 +78,11 @@ class CreateUserViewSet(CreateAPIView):
 
         return Response(response_data, status=status.HTTP_201_CREATED)
 
-
 class LoginView(GenericAPIView):
     serializer_class = UserLoginSerializer
     permission_classes = [AllowAny]
+    @extend_schema(tags=["Students Auth"], operation_id="Student login")
 
-    @swagger_auto_schema(tags=["Students Auth"], operation_id="Student login")
     def post(self, request):
         obj = request.data
         serializer = self.serializer_class(data=obj)
@@ -103,26 +102,29 @@ class LoginView(GenericAPIView):
             raise exceptions.AuthenticationFailed(data)
 
         refresh = RefreshToken.for_user(user)
-
+        
         refresh['user_role'] = user.user_role
-
+        
         access = refresh.access_token
-        expiration_time = datetime.datetime.fromtimestamp(
-            access['exp'], tz=datetime.timezone.utc)
-        access_exp = (expiration_time -
-                      datetime.datetime.now(tz=datetime.timezone.utc)).seconds
-        data = {
-            "user": user.profile(),
-            "token": {"access": str(access), "refresh": str(refresh), "expires_in_secs": str(access_exp)},
-        }
+        expiration_time = datetime.datetime.fromtimestamp(access['exp'], tz=datetime.timezone.utc)
+        access_exp = (expiration_time - datetime.datetime.now(tz=datetime.timezone.utc)).seconds
+        data = user.profile()
+        data.update({"access": str(access), "refresh": str(refresh), "expires_in_secs": str(access_exp)})
 
         return Response(data, status.HTTP_200_OK)
 
+@extend_schema(
+    tags=["Students Auth"],
+    operation_id="Refresh access token"
+)
+class CustomTokenRefreshView(TokenRefreshView):
+    """Students can refresh their access token using refresh token"""
+    pass
 
 class ForgotPassword(GenericAPIView):
     serializer_class = ResendEmailActivationSerializer
+    @extend_schema(tags=["Password validators"], operation_id="Forgot password")
 
-    @swagger_auto_schema(tags=["Password validators"], operation_id="Forgot password")
     def post(self, request):
         obj = request.data
         serializer = self.serializer_class(data=obj)
@@ -142,7 +144,7 @@ class ForgotPassword(GenericAPIView):
             "email": user_data['email']
         })
 
-        subject = 'Talk: Password Reset Link'
+        subject ='Talk: Password Reset Link'
 
         thread = threading.Thread(
             target=send_email_in_thread,
@@ -153,11 +155,11 @@ class ForgotPassword(GenericAPIView):
         user_data["message"] = "password reset link sent successfully"
         return Response(user_data, status.HTTP_201_CREATED)
 
-
 class SetNewPassword(GenericAPIView):
     serializer_class = SetNewPasswordSerializer
+    @extend_schema(tags=["Password validators"], operation_id="Set new password")
 
-    @swagger_auto_schema(tags=["Password validators"], operation_id="Set new password")
+
     def post(self, request):
         obj = request.data
         serializer = self.serializer_class(data=obj)
@@ -167,8 +169,7 @@ class SetNewPassword(GenericAPIView):
         token = user_data["token"]
         password = user_data["password"]
         try:
-            payload = jwt.decode(
-                token, settings.SECRET_KEY, algorithms=["HS256"])
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
             user = User.objects.get(id=payload["user_id"])
             user.set_password(password)
             user.save()
@@ -184,11 +185,10 @@ class SetNewPassword(GenericAPIView):
                 {"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST
             )
 
-
 class VerifyOTP(GenericAPIView):
     serializer_class = OTPVerificationSerializer
+    @extend_schema(tags=["OTP Verification"], operation_id="Verify user with `OTP`")
 
-    @swagger_auto_schema(tags=["OTP Verification"], operation_id="Verify user with `OTP`")
     def post(self, request):
         obj = request.data
         serializer = self.serializer_class(data=obj)
@@ -215,34 +215,29 @@ class VerifyOTP(GenericAPIView):
             data = {"user": user.profile()}
 
             return Response(
-                custom_response(status.HTTP_200_OK, status="success",
-                                mssg="User verified successfully", data=data),
+                custom_response(status.HTTP_200_OK, status="success", mssg="User verified successfully", data=data),
                 status=status.HTTP_200_OK,
             )
         except OneTimePassword.DoesNotExist:
             return Response(
-                custom_response(status.HTTP_400_BAD_REQUEST,
-                                status="failed", mssg="Invalid OTP", data={}),
+                custom_response(status.HTTP_400_BAD_REQUEST, status="failed", mssg="Invalid OTP", data={}),
                 status=status.HTTP_400_BAD_REQUEST
             )
         except User.DoesNotExist:
             return Response(
-                custom_response(status.HTTP_400_BAD_REQUEST,
-                                status="failed", mssg="User not found", data={}),
+                custom_response(status.HTTP_400_BAD_REQUEST, status="failed", mssg="User not found", data={}),
                 status=status.HTTP_400_BAD_REQUEST
             )
         except Exception as e:
             return Response(
-                custom_response(status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                status="failed", mssg=str(e), data={}),
+                custom_response(status.HTTP_500_INTERNAL_SERVER_ERROR, status="failed", mssg=str(e), data={}),
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
-
 class ResendOTP(GenericAPIView):
-    serializer_class = ResendEmailActivationSerializer
+    serializer_class  = ResendEmailActivationSerializer
+    @extend_schema(tags=["OTP Verification"], operation_id="Resend OTP code")
 
-    @swagger_auto_schema(tags=["OTP Verification"], operation_id="Resend OTP code")
     def post(self, request):
         obj = request.data
         serializer = self.serializer_class(data=obj)
@@ -262,8 +257,7 @@ class ResendOTP(GenericAPIView):
             })
 
             subject = "One time PWD"
-            thread = threading.Thread(target=send_email_in_thread, args=(
-                subject, email_data, user.email))
+            thread = threading.Thread(target=send_email_in_thread, args=(subject, email_data, user.email))
             thread.start()
 
             user_data["message"] = "Email activation link sent successfully"
@@ -276,16 +270,30 @@ class ResendOTP(GenericAPIView):
             user_data["message"] = f"{e}"
             return Response(user_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+class UserProfileViewSet(RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(tags=["Profile Updates"], operation_id="View user profile")
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        return Response({
+            "status": "success",
+            "code": status.HTTP_200_OK,
+            "message": "User profile retrieved successfully",
+            "data": user.profile(),
+        }, status=status.HTTP_200_OK)
 
 class UpdateUserProfileViewSet(UpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UpdateUserProfileSerializer
     permission_classes = [IsAuthenticated]
     http_method_names = ["patch"]
-
-    @swagger_auto_schema(tags=["Profile Updates"], operation_id="Update user profile")
+    @extend_schema(tags=["Profile Updates"], operation_id="Update user profile")
+    
     # def get_object(self):
     #     return self.request.user
+    
     def patch(self, request, *args, **kwargs):
         user = self.request.user
         serializer = self.get_serializer(user, data=request.data, partial=True)
@@ -298,14 +306,13 @@ class UpdateUserProfileViewSet(UpdateAPIView):
             "data": serializer.data,
         }, status=status.HTTP_200_OK)
 
-
 class CreateIndividualViewSet(CreateAPIView):
     serializer_class = IndividualSerializer
     queryset = Individual.objects.all()
     permission_classes = [IsAuthenticated]
     http_method_names = ["post"]
+    @extend_schema(tags=["Profile Updates"], operation_id="Create individual profile")
 
-    @swagger_auto_schema(tags=["Profile Updates"], operation_id="Create individual profile")
     def post(self, request, *args, **kwargs):
         try:
             serializer = self.get_serializer(data=request.data)
@@ -324,14 +331,13 @@ class CreateIndividualViewSet(CreateAPIView):
                 "message": str(e),
             }, status=status.HTTP_400_BAD_REQUEST)
 
-
 class CreateServiceProvidersViewSet(CreateAPIView):
     serializer_class = ServiceProvidersSerializer
     queryset = ServiceProvider.objects.all()
     permission_classes = [IsAuthenticated]
     http_method_names = ["post"]
+    @extend_schema(tags=["Profile Updates"], operation_id="Create Service Provider's Profile")
 
-    @swagger_auto_schema(tags=["Profile Updates"], operation_id="Create Service Provider's Profile")
     def post(self, request, *args, **kwargs):
         try:
             serializer = self.get_serializer(data=request.data)
@@ -350,14 +356,13 @@ class CreateServiceProvidersViewSet(CreateAPIView):
                 "message": str(e)
             }, status=status.HTTP_400_BAD_REQUEST)
 
-
 class UpdateIndividualProfileViewSet(UpdateAPIView):
     serializer_class = IndividualSerializer
     queryset = Individual.objects.all()
     permission_classes = [IsAuthenticated]
     http_method_names = ["patch"]
-
-    @swagger_auto_schema(tags=["Profile Updates"], operation_id="Update individual profile")
+    @extend_schema(tags=["Profile Updates"], operation_id="Update individual profile")
+    
     def patch(self, request, *args, **kwargs):
         user = Individual.objects.get(user=self.request.user)
         serializer = self.get_serializer(user, data=request.data, partial=True)
@@ -370,14 +375,13 @@ class UpdateIndividualProfileViewSet(UpdateAPIView):
             "data": serializer.data,
         }, status=status.HTTP_200_OK)
 
-
 class UpdateServiceProviderProfileViewSet(UpdateAPIView):
     serializer_class = ServiceProvidersSerializer
     queryset = ServiceProvider.objects.all()
     permission_classes = [IsAuthenticated]
     http_method_names = ["patch"]
-
-    @swagger_auto_schema(tags=["Profile Updates"], operation_id="Update Service provider's profile")
+    @extend_schema(tags=["Profile Updates"], operation_id="Update Service provider's profile")
+    
     def patch(self, request, *args, **kwargs):
         user = ServiceProvider.objects.get(user=self.request.user)
         serializer = self.get_serializer(user, data=request.data, partial=True)
@@ -389,3 +393,29 @@ class UpdateServiceProviderProfileViewSet(UpdateAPIView):
             "message": "Service provider profile updated successfully",
             "data": serializer.data,
         }, status=status.HTTP_200_OK)
+
+class RetrieveServiceProviderProfileViewSet(GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated]
+    http_method_names = ["get"]
+    lookup_field = "id"
+
+    @extend_schema(tags=["Profile Updates"], operation_id="View Service provider's profile")
+
+    def get(self, request, id=None):
+        queryset = self.get_queryset()
+        try:
+            instance = queryset.get(id=id).profile()
+            return Response({
+                "status": "success",
+                "code": status.HTTP_200_OK,
+                "message": "Service provider profile retrieved successfully",
+                "data": instance
+            }, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist as e:
+            print("Error occurred:", e)
+            return Response({
+                "status": "failed",
+                "code": status.HTTP_404_NOT_FOUND,
+                "message": str(e)
+            }, status=status.HTTP_404_NOT_FOUND)
