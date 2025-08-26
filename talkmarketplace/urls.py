e00707f8a73a02b0d3422c43cb4dc47d5270023d
Dilosi Richfield <richiedilosi2003@gmail.com>
2025-08-26 14:29:15 +0100

    Update urls.py

--- talkmarketplace/urls.py
+++ talkmarketplace/urls.py
@@ -3,12 +3,14 @@ from .views import (
     MarketPlaceProductCreateView,
     ListMarketPlaceProductsView,
     ProvidersMarketPlaceProductListView,
+    MarketPlaceProductDetailView,
     MarketPlaceProductUpdateView,
     MarketPlaceProductDeleteView,
 
     TakaProductCreateView,
     ListTakaProductsView,
     ProvidersTakaProductListView,
+    TakaProductDetailView,
     TakaProductUpdateView,
     TakaProductDeleteView,
     SaveItemView
@@ -17,17 +19,19 @@ from .views import (
 marketplace_urlpatterns = [
     path('create-product/', MarketPlaceProductCreateView.as_view(), name='create_product'),
     path('list-products/', ListMarketPlaceProductsView.as_view(), name='list_products'),
+    path('product-detail/<slug:slug>/', MarketPlaceProductDetailView.as_view(), name='product_detail'),
     path('providers-products/', ProvidersMarketPlaceProductListView.as_view(), name='providers_products'),
     path('update-product/<slug:slug>/', MarketPlaceProductUpdateView.as_view(), name='product_detail'),
-    path('delete-product/<int:id>/', MarketPlaceProductDeleteView.as_view(), name='delete_product'),
+    path('delete-product/<str:id>/', MarketPlaceProductDeleteView.as_view(), name='delete_product'),
 ]
 
 taka_urlpatterns = [
     path('create-product/', TakaProductCreateView.as_view(), name='create_product'),
     path('list-products/', ListTakaProductsView.as_view(), name='list_products'),
+    path('product-detail/<slug:slug>/', TakaProductDetailView.as_view(), name='product_detail'),
     path('providers-products/', ProvidersTakaProductListView.as_view(), name='providers_products'),
     path('update-product/<slug:slug>/', TakaProductUpdateView.as_view(), name='product_detail'),
-    path('delete-product/<int:id>/', TakaProductDeleteView.as_view(), name='delete_product'),
+    path('delete-product/<str:id>/', TakaProductDeleteView.as_view(), name='delete_product'),
 ]
 
 urlpatterns = [
