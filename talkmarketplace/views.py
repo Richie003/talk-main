33513e40ec0e02a8a471c20ecfdb1a38ce79f178
Dilosi Richfield <richiedilosi2003@gmail.com>
2025-09-02 12:45:04 +0100

    Updated views.py

--- talkmarketplace/views.py
+++ talkmarketplace/views.py
@@ -9,12 +9,14 @@ from .models import (
     Product,
     MarketPlaceProduct,
     TakaProduct,
-    SavedItem
+    SavedItem,
+    Service
 )
 from .serializers import (
     MarketPlaceProductSerializer, 
     TakaProductSerializer,
     SavedItemsSerializer,
+    ServiceSerializer
 )
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
@@ -22,14 +24,14 @@ from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import exceptions, status
 from django.shortcuts import get_object_or_404
-from drf_yasg import openapi
 from rest_framework.parsers import MultiPartParser, FormParser
 from utils.helpers import custom_response
-from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiParameter
+from drf_spectacular.utils import extend_schema
 
 tag_names = {
     "marketplace": "Marketplace",
     "taka": "Taka",
+    "services": "Services",
     "inventory": "Inventory",
 }
 
@@ -70,41 +72,6 @@ class SaveItemView(GenericAPIView):
                 data=None
             ), status=status.HTTP_404_NOT_FOUND)
 
-class DeleteSavedItemView(GenericAPIView):
-    """
-        Deletes a saved product for the authenticated user
-    """
-    permission_classes = [IsAuthenticated]
-
-    @extend_schema(tags=[tag_names["inventory"]], operation_id="Delete a saved item")
-    def delete(self, request, *args, **kwargs):
-        product_id = kwargs.get('product_id')
-        user = request.user
-
-        try:
-            product = get_object_or_404(Product, id=product_id)
-            saved_instance = SavedItem.objects.get(user=user)
-
-            if saved_instance.is_item_saved(product):
-                saved_instance.remove_item(product)
-                return Response(custom_response(
-                    status_mthd=status.HTTP_204_NO_CONTENT,
-                    status="success",
-                    mssg="Item removed from saved list"
-                ), status=status.HTTP_204_NO_CONTENT)
-
-            return Response(custom_response(
-                status_mthd=status.HTTP_404_NOT_FOUND,
-                status="error",
-                mssg="Item was not in saved list"
-            ), status=status.HTTP_404_NOT_FOUND)
-
-        except SavedItem.DoesNotExist:
-            return Response(custom_response(
-                status_mthd=status.HTTP_404_NOT_FOUND,
-                status="error",
-                mssg="No saved items for user"
-            ), status=status.HTTP_404_NOT_FOUND)
 
 class MarketPlaceProductCreateView(GenericAPIView):
     serializer_class = MarketPlaceProductSerializer
@@ -505,6 +472,206 @@ class TakaProductDeleteView(GenericAPIView):
                 status="success",
                 mssg="Product deleted successfully",
                 data = {}
+            ),
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+#### ---- SERVICES ---- ####
+class ServiceCreateView(GenericAPIView):
+    serializer_class = ServiceSerializer
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
+    @extend_schema(
+        tags=[tag_names["services"]], 
+        operation_id="Create and upload a service"
+    )
+    def post(self, request, *args, **kwargs):
+        data = request.data.copy()
+        try:
+            serializer = self.get_serializer(
+                data=data,
+                context={"request": request}
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response(
+                custom_response(
+                    status_mthd=status.HTTP_201_CREATED,
+                    status="success",
+                    mssg="Service created successfully",
+                    data=serializer.data
+                ),
+                status=status.HTTP_201_CREATED
+            )
+
+        except Exception as e:
+            return Response(
+                custom_response(
+                    status_mthd=status.HTTP_400_BAD_REQUEST,
+                    status="error",
+                    mssg="Failed to create service",
+                    data=str(e)
+                ),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+class ListServicesView(ListAPIView):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
+
+    @extend_schema(tags=[tag_names["services"]], operation_id="Get all services")
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            data = [service.service_profile() for service in page]
+            return self.get_paginated_response(
+                custom_response(
+                    status_mthd=status.HTTP_200_OK,
+                    status="success",
+                    mssg="Data retrieved successfully",
+                    data=data
+                )
+            )
+
+        serializer = self.get_serializer(queryset, many=True)
+        if not serializer.data:
+            raise exceptions.NotFound("No products found")
+
+        data = [service.service_profile() for service in queryset]
+        return Response(
+            custom_response(
+                status_mthd=status.HTTP_200_OK,
+                status="success",
+                mssg="Data retrieved successfully",
+                data=data
+            ),
+            status=status.HTTP_200_OK
+        )
+
+class ProvidersServicesListView(GenericAPIView):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
+
+    @extend_schema(tags=[tag_names["services"]], operation_id="Get services of a service provider")
+    def get(self, request, *args, **kwargs):
+        user_id = request.user.id
+        if not user_id:
+            raise exceptions.NotAuthenticated("User not authenticated")
+        
+        queryset = Service.objects.filter(user=user_id)
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            data = [service.service_profile() for service in page]
+            return self.get_paginated_response(
+                custom_response(
+                    status_mthd=status.HTTP_200_OK,
+                    status="success",
+                    mssg="Data retrieved successfully",
+                    data=data
+                )
+            )
+        
+        serializer = self.get_serializer(queryset, many=True)
+        if not serializer.data:
+            raise exceptions.NotFound("No service found")
+        
+        data = [service.service_profile() for service in queryset]
+        return Response(
+            custom_response(
+                status_mthd=status.HTTP_200_OK,
+                status="success",
+                mssg="Data retrieved successfully",
+                data=data
+            ),
+            status=status.HTTP_200_OK
+        )
+
+class ServiceDetailView(GenericAPIView):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.all()
+    permission_classes = [IsAuthenticated]
+    lookup_field = "slug"
+    http_method_names = ["get"]
+
+    @extend_schema(tags=[tag_names["services"]], operation_id="Get a service details")
+    def get(self, request, *args, **kwargs):
+        slug = kwargs.get('slug')
+        try:
+            service = self.queryset.get(slug=slug)
+        except ObjectDoesNotExist:
+            raise exceptions.NotFound("Service not Found")
+        
+        serializer = self.get_serializer(service)
+        return Response(
+            custom_response(
+                status_mthd=status.HTTP_200_OK,
+                status="success",
+                mssg="Service detail retrieved successfully",
+                data=serializer.data
+            ),
+            status=status.HTTP_200_OK
+        )
+
+class ServiceUpdateView(UpdateAPIView):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.all()
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+    lookup_field = "slug"
+    http_method_names = ["patch"]
+
+    @extend_schema(tags=[tag_names["services"]], operation_id="Update a service")
+    def patch(self, request, *args, **kwargs):
+        slug = kwargs.get("slug")
+        try:
+            service = self.queryset.get(slug=slug)
+        except ObjectDoesNotExist:
+            raise exceptions.NotFound("Service not found")
+        
+        serializer = self.get_serializer(service, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(
+            custom_response(
+                status_mthd=status.HTTP_200_OK,
+                status="success",
+                mssg="Service updated successfully",
+                data=serializer.data
+            ),
+            status=status.HTTP_200_OK
+        )
+    
+class ServiceDeleteView(GenericAPIView):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.all()
+    permission_classes = [IsAuthenticated]
+    lookup_field = "id"
+
+    @extend_schema(tags=[tag_names["services"]], operation_id="Delete a service")
+    def delete(self, request, *args, **kwargs):
+        id = kwargs.get("id")
+        try:
+            service = self.queryset.get(id=id)
+        except ObjectDoesNotExist:
+            raise exceptions.NotFound("Service not found")
+
+        service.delete()
+        return Response(
+            custom_response(
+                status_mthd=status.HTTP_204_NO_CONTENT,
+                status="success",
+                mssg="Service deleted successfully",
+                data=None
             ),
             status=status.HTTP_204_NO_CONTENT
         )
